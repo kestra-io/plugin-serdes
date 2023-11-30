c5513fa27bcb94fea03969a93b8306e187383279
brian.mulier <bmmulier@hotmail.fr>
2023-11-30 17:40:31 +0100

    fix(excel): properly handling ion number format retrieved as Long, BigDecimal or BigIntegers

--- src/test/java/io/kestra/plugin/serdes/excel/IonToExcelTest.java
+++ src/test/java/io/kestra/plugin/serdes/excel/IonToExcelTest.java
@@ -2,8 +2,6 @@ package io.kestra.plugin.serdes.excel;
 
 import bad.robot.excel.matchers.WorkbookMatcher;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.io.CharStreams;
-import io.kestra.core.models.executions.metrics.Counter;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.storages.StorageInterface;
@@ -11,15 +9,12 @@ import io.kestra.core.utils.TestsUtils;
 import io.kestra.plugin.serdes.SerdesUtils;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.net.URI;
-import java.time.ZoneId;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -35,40 +30,31 @@ public class IonToExcelTest {
     @Inject
     SerdesUtils serdesUtils;
 
-    private void test(String file, boolean header) throws Exception {
-        File sourceFile = SerdesUtils.resourceToFile(file);
-        URI source = this.serdesUtils.resourceToStorageObject(sourceFile);
-
-        ExcelToIon reader = ExcelToIon.builder()
-            .id(ExcelToIonTest.class.getSimpleName())
-            .type(ExcelToIon.class.getName())
-            .from(source.toString())
-            .header(header)
-            .build();
-        ExcelToIon.Output ionOutput = reader.run(TestsUtils.mockRunContext(runContextFactory, reader, ImmutableMap.of()));
+    private void test(String inputIonResourcePath, String expectedExcelResourcePath, boolean header) throws Exception {
+        URI inputUri = this.serdesUtils.resourceToStorageObject(SerdesUtils.resourceToFile(inputIonResourcePath));
 
         IonToExcel writer = IonToExcel.builder()
             .id(IonToExcelTest.class.getSimpleName())
             .type(IonToExcel.class.getName())
             .sheetsTitle("Worksheet")
-            .from(ionOutput.getUris().get("Worksheet").toString())
+            .from(inputUri.toString())
             .header(header)
             .build();
         IonToExcel.Output excelOutput = writer.run(TestsUtils.mockRunContext(runContextFactory, writer, ImmutableMap.of()));
 
         XSSFWorkbook actual = new XSSFWorkbook(storageInterface.get(null, excelOutput.getUri()));
-        XSSFWorkbook expected = new XSSFWorkbook(new FileInputStream(sourceFile));
+        XSSFWorkbook expected = new XSSFWorkbook(new FileInputStream(SerdesUtils.resourceToFile(expectedExcelResourcePath)));
         assertThat(actual, WorkbookMatcher.sameWorkbook(expected));
     }
 
     @Test
     void header() throws Exception {
-        this.test("excel/insurance_sample.xlsx", true);
+        this.test("excel/insurance_sample.ion", "excel/insurance_sample.xlsx", true);
     }
 
     @Test
     void noHeader() throws Exception {
-        this.test("excel/insurance_sample_no_header.xlsx", false);
+        this.test("excel/insurance_sample.ion", "excel/insurance_sample_no_header.xlsx", false);
     }
 
     @Test
