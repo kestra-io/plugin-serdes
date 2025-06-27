3fea8a3aadac4dbecb3296772d257839499f1e49
François Delbrayelle <fdelbrayelle@gmail.com>
2025-06-27 14:50:37 +0200

    fix(excel): unexpected date time instead of date

--- src/test/java/io/kestra/plugin/serdes/excel/ExcelToIonTest.java
+++ src/test/java/io/kestra/plugin/serdes/excel/ExcelToIonTest.java
@@ -11,6 +11,9 @@ import io.kestra.core.utils.TestsUtils;
 import io.kestra.plugin.serdes.SerdesUtils;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -18,6 +21,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -33,55 +37,77 @@ public class ExcelToIonTest {
     @Inject
     SerdesUtils serdesUtils;
 
-    @Test
-    void ion() throws Exception {
+    @ParameterizedTest
+    @MethodSource("should_get_a_correct_ion_inputs")
+    void should_get_a_correct_ion(String excelFile, String excelSheet, Boolean withHeaders, List<String> expectedStrings) throws Exception {
         File tempFile = File.createTempFile(this.getClass().getSimpleName().toLowerCase() + "_", ".ion");
-        try (OutputStream output = new FileOutputStream(tempFile)) {
+        try (OutputStream unused = new FileOutputStream(tempFile)) {
 
-            File sourceFile = SerdesUtils.resourceToFile("excel/insurance_sample.xlsx");
+            File sourceFile = SerdesUtils.resourceToFile(excelFile);
             URI source = this.serdesUtils.resourceToStorageObject(sourceFile);
 
             ExcelToIon reader = ExcelToIon.builder()
                 .id(ExcelToIonTest.class.getSimpleName())
                 .type(ExcelToIon.class.getName())
                 .from(Property.ofValue(source.toString()))
-                .header(Property.ofValue(true))
+                .header(Property.ofValue(withHeaders))
                 .build();
             ExcelToIon.Output ionOutput = reader.run(TestsUtils.mockRunContext(runContextFactory, reader, ImmutableMap.of()));
 
-            String out = CharStreams.toString(new InputStreamReader(storageInterface.get(TenantService.MAIN_TENANT, null, ionOutput.getUris().get("Worksheet"))));
+            String out = CharStreams.toString(new InputStreamReader(storageInterface.get(TenantService.MAIN_TENANT, null, ionOutput.getUris().get(excelSheet))));
 
-            assertThat(out, containsString("policyID:\"333743\""));
-            assertThat(out, containsString("point_latitude:30.102261"));
+            expectedStrings.forEach(expectedString -> assertThat(out, containsString(expectedString)));
         }
     }
 
-    @Test
-    void ion_without_header() throws Exception {
-        File tempFile = File.createTempFile(this.getClass().getSimpleName().toLowerCase() + "_", ".ion");
-        try (OutputStream output = new FileOutputStream(tempFile)) {
-
-            File sourceFile = SerdesUtils.resourceToFile("excel/insurance_sample.xlsx");
-            URI source = this.serdesUtils.resourceToStorageObject(sourceFile);
-
-            ExcelToIon reader = ExcelToIon.builder()
-                .id(ExcelToIonTest.class.getSimpleName())
-                .type(ExcelToIon.class.getName())
-                .from(Property.ofValue(source.toString()))
-                .header(Property.ofValue(false))
-                .build();
-            ExcelToIon.Output ionOutput = reader.run(TestsUtils.mockRunContext(runContextFactory, reader, ImmutableMap.of()));
-
-            String out = CharStreams.toString(new InputStreamReader(storageInterface.get(TenantService.MAIN_TENANT, null, ionOutput.getUris().get("Worksheet"))));
-
-            assertThat(out, containsString("\"policyID\",\"statecode\""));
-        }
+    private static Stream<Arguments> should_get_a_correct_ion_inputs() {
+        return Stream.of(
+            Arguments.of(
+                "excel/insurance_sample.xlsx",
+                "Worksheet",
+                true,
+                List.of(
+                    "policyID:\"333743\"",
+                    "point_latitude:30.102261"
+                )
+            ),
+            Arguments.of(
+                "excel/insurance_sample.xlsx",
+                "Worksheet",
+                false,
+                List.of("\"policyID\",\"statecode\"")
+            ),
+            Arguments.of(
+                "excel/missing_cells_sample.xlsx",
+                "Sheet1",
+                true,
+                List.of("abc")
+            ),
+            Arguments.of(
+                "excel/sample_with_a_full_missing_column.xlsx",
+                "Sheet1",
+                true,
+                List.of("PrizeMasterType:null")
+            ),
+            Arguments.of(
+                "excel/sample_with_date.xlsx",
+                "Sheet1",
+                true,
+                List.of("2025-04-01")
+            ),
+            Arguments.of(
+                "excel/sample_with_date.xlsx",
+                "Sheet1",
+                true,
+                List.of("2025-06-27 14h22")
+            )
+        );
     }
 
     @Test
     void multiSheets() throws Exception {
         File tempFile = File.createTempFile(this.getClass().getSimpleName().toLowerCase() + "_", ".ion");
-        try (OutputStream output = new FileOutputStream(tempFile)) {
+        try (OutputStream unused = new FileOutputStream(tempFile)) {
 
             File sourceFile = SerdesUtils.resourceToFile("excel/insurance_sample_multiple_sheets.xlsx");
             URI source = this.serdesUtils.resourceToStorageObject(sourceFile);
@@ -146,50 +172,6 @@ public class ExcelToIonTest {
 
             assertThat(outWorkSheet3, containsString("policyID:\"333743\""));
             assertThat(outWorkSheet3, containsString("point_latitude:30.102261"));
-        }
-    }
-
-    @Test
-    void ion_with_missing_cells() throws Exception {
-        File tempFile = File.createTempFile(this.getClass().getSimpleName().toLowerCase() + "_", ".ion");
-        try (OutputStream unused = new FileOutputStream(tempFile)) {
-
-            File sourceFile = SerdesUtils.resourceToFile("excel/missing_cells_sample.xlsx");
-            URI source = this.serdesUtils.resourceToStorageObject(sourceFile);
-
-            ExcelToIon reader = ExcelToIon.builder()
-                .id(ExcelToIonTest.class.getSimpleName())
-                .type(ExcelToIon.class.getName())
-                .from(Property.ofValue(source.toString()))
-                .header(Property.ofValue(true))
-                .build();
-            ExcelToIon.Output ionOutput = reader.run(TestsUtils.mockRunContext(runContextFactory, reader, ImmutableMap.of()));
-
-            String out = CharStreams.toString(new InputStreamReader(storageInterface.get(TenantService.MAIN_TENANT, null, ionOutput.getUris().get("Sheet1"))));
-
-            assertThat(out, containsString("abc"));
-        }
-    }
-
-    @Test
-    void ion_with_empty_column() throws Exception {
-        File tempFile = File.createTempFile(this.getClass().getSimpleName().toLowerCase() + "_", ".ion");
-        try (OutputStream unused = new FileOutputStream(tempFile)) {
-
-            File sourceFile = SerdesUtils.resourceToFile("excel/zendesk498.xlsx");
-            URI source = this.serdesUtils.resourceToStorageObject(sourceFile);
-
-            ExcelToIon reader = ExcelToIon.builder()
-                .id(ExcelToIonTest.class.getSimpleName())
-                .type(ExcelToIon.class.getName())
-                .from(Property.ofValue(source.toString()))
-                .header(Property.ofValue(true))
-                .build();
-            ExcelToIon.Output ionOutput = reader.run(TestsUtils.mockRunContext(runContextFactory, reader, ImmutableMap.of()));
-
-            String out = CharStreams.toString(new InputStreamReader(storageInterface.get(TenantService.MAIN_TENANT, null, ionOutput.getUris().get("Sheet1"))));
-
-            assertThat(out, containsString("PrizeMasterType:null"));
         }
     }
 }
