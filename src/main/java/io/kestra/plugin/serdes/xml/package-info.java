72ac6a6a2358c9d3bff929859011779cac167df7
Malaydewangan09 <malaydewangan310@gmail.com>
2025-08-12 22:56:59 +0530

    fix(): subgroups for better readability

--- src/main/java/io/kestra/plugin/serdes/xml/package-info.java
+++ src/main/java/io/kestra/plugin/serdes/xml/package-info.java
@@ -1,4 +1,5 @@
 @PluginSubGroup(
+    title = "XML",
     description = "This sub-group of plugins contains tasks for serialization and deserialization of files in the XML format.",
     categories = PluginSubGroup.PluginCategory.TRANSFORMATION
 )
