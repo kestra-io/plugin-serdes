ce65e3ae79db6d4e45e65f74e320114c899532c4
Malay Dewangan <malaydewangan310@gmail.com>
2025-11-29 01:50:02 +0530

    feat(yaml): introduce YAML tasks

---
+++ src/main/java/io/kestra/plugin/serdes/yaml/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "YAML",
+    description = "This sub-group of plugins contains tasks for serialization and deserialization of files in the YAML format.",
+    categories = PluginSubGroup.PluginCategory.TRANSFORMATION
+)
+package io.kestra.plugin.serdes.yaml;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
