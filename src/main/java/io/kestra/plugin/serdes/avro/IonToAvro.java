1a2d233d1209fd3e84972b57c81995a468013696
Rishi Jat <rishijat098@gmail.com>
2026-01-07 12:35:53 +0530

    feat(serdes): infer Parquet schema from ION when schema is missing
    Signed-off-by: Rishi Jat <rishijat098@gmail.com>

--- src/main/java/io/kestra/plugin/serdes/avro/IonToAvro.java
+++ src/main/java/io/kestra/plugin/serdes/avro/IonToAvro.java
@@ -130,12 +130,19 @@ public class IonToAvro extends AbstractAvroConverter implements RunnableTask<Ion
 
         // avro writer
         var schemaParser = new Schema.Parser();
-        Schema schema = null;
+        Schema schema;
         if (this.schema == null) {
-            var inputStreamForInfer = new InputStreamReader(runContext.storage().getFile(rFrom));
-            var schemaOutputStream = new ByteArrayOutputStream();
-            new InferAvroSchema().inferAvroSchemaFromIon(inputStreamForInfer, schemaOutputStream);
-            schema = schemaParser.parse(schemaOutputStream.toString());
+            try (var inputStreamForInfer = new InputStreamReader(runContext.storage().getFile(rFrom))) {
+                var schemaOutputStream = new ByteArrayOutputStream();
+                new InferAvroSchema(
+                    runContext.render(this.getNumberOfRowsToScan()).as(Integer.class).orElse(100)
+                ).inferAvroSchemaFromIon(inputStreamForInfer, schemaOutputStream);
+                String inferredSchemaString = schemaOutputStream.toString();
+                if (inferredSchemaString.isEmpty()) {
+                    throw new IllegalStateException("Cannot infer Avro schema from ION input: the file appears to be empty or contains no valid records.");
+                }
+                schema = schemaParser.parse(inferredSchemaString);
+            }
         } else {
             try {
                 schema = schemaParser.parse(runContext.render(this.schema));
